b9ec2ce5c9faddbce5b66ab615082540d9a615af
eduardodev404 <eduardodev2005@gmail.com>
2025-04-05 02:25:01 -0300

    descarteupdate

--- src/pages/Descarte/Informativo.jsx
+++ src/pages/Descarte/Informativo.jsx
@@ -5,45 +5,66 @@ import chemicalSymbol from "../../img/residuoQuimico.png";
 import radioactiveSymbol from "../../img/residuoRadioativo.png";
 import commomSymbol from "../../img/residuoComum.png";
 import sharpSymbol from "../../img/residuoPerfurocortante.png";
+import { Link } from 'react-router-dom';
 
 function Informativo() {
     return (
         <div className={styles.secaoResiduos}>
             
             <div className={styles.containerBotoes}>
-                <div className={styles.containerResiduo}>
-                    <button>
-                        <img src={biologicalSymbol} alt="símbolo risco infectante" />
-                        <h2>Resíduo Infectante</h2>
-                    </button>
+                <div className={styles.containerResiduo}>  
+                     <div className="infectante">
+                        <Link to="/infectante">
+                            <button>
+                                <img src={biologicalSymbol} alt="símbolo risco infectante" />
+                                <h2>Resíduo Infectante</h2>
+                            </button>
+                        </Link>
+                </div>
                 </div>
                 
                 <div className={styles.containerResiduo}>
-                    <button>
-                        <img src={chemicalSymbol} alt="símbolo risco químico" />
-                        <h2>Resíduo Químico</h2>
-                    </button>
+                    <div className="quimico">
+                        <Link to="/quimico">
+                            <button>
+                                <img src={chemicalSymbol} alt="símbolo risco químico" />
+                                <h2>Resíduo Químico</h2>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
                 
                 <div className={styles.containerResiduo}>
-                    <button>
-                        <img src={radioactiveSymbol} alt="símbolo risco radioativo" />
-                        <h2>Resíduo Radioativo</h2>
-                    </button>
+                <div className="radioativo">
+                        <Link to="/radioativo">
+                            <button>
+                                <img src={radioactiveSymbol} alt="símbolo risco radioativo" />
+                                <h2>Resíduo Radioativo</h2>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
                 
                 <div className={styles.containerResiduo}>
-                    <button>
-                        <img src={commomSymbol} alt="símbolo resíduos comuns" />
-                        <h2>Resíduos Comuns</h2>
-                    </button>
+                <div className="comuns">
+                        <Link to="/comuns">
+                            <button>
+                                <img src={commomSymbol} alt="símbolo resíduos comuns" />
+                                <h2>Resíduos Comuns</h2>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
                 
                 <div className={styles.containerResiduo}>
-                    <button>
-                        <img src={sharpSymbol} alt="símbolo resíduos perfurocortantes" />
-                        <h2>Resíduo Perfurocortante</h2>
-                    </button>
+                <div className="cortante">
+                        <Link to="/cortante">
+                            <button>
+                                <img src={sharpSymbol} alt="símbolo resíduos perfurocortantes" />
+                                <h2>Resíduo Perfurocortante</h2>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div> 
 
